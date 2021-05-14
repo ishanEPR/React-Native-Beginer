f9b393889958fce80b33a11c038a76351c54656d
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-05-14 18:44:05 +0530

    add move to one screen to another screen

--- Screen1.js
+++ Screen1.js
@@ -3,12 +3,18 @@ import {View,Text,Button} from 'react-native';
 
 export default class screen1 extends Component{
 
+
+    show=()=>
+    {
+        this.props.navigation.navigate('S2');
+    }
+
     render()
     {
-        return(
+        return( 
             <View>
                 <Text style={{textAlign:'center',fontSize:20}}>screen</Text>
-                <Button title="show"></Button>
+                <Button title="show" onPress={this.show}></Button>
             </View>
         );
     }
--- Screen2.js
+++ Screen2.js
@@ -3,12 +3,17 @@ import {View,Text,Button} from 'react-native';
 
 export default class screen2 extends Component{
 
+    show=()=>
+    {
+        this.props.navigation.navigate('S1');
+    }
     render()
     {
         return(
             <View>
                 <Text style={{textAlign:'center',fontSize:20}}>screen</Text>
-                
+                <Button title="click" onPress={this.show}/>
+                 
             </View>
         );
     }
