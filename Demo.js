8ab4b5d777c979a883deca8b42629e0d52a5e9d8
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-05-14 17:01:14 +0530

    sum the two numbers

--- Demo.js
+++ Demo.js
@@ -1,14 +1,31 @@
 import React,{Component} from 'react';
-import {View,Text} from 'react-native';
+import {View,Button,TextInput} from 'react-native';
 
 
 export default class Demo extends Component
 {
+
+    constructor(props)
+    {
+        super(props);
+        this.state={Num1:0,Num2:0};
+    }
+
+    F1=()=>
+    {
+        var num1=parseInt(this.state.Num1);
+        var num2=parseInt(this.state.Num2);
+        var R=num1+num2;
+        alert(R);
+    }
+
     render()
     {
         return(
             <View>
-                  <Text>Hello ishan</Text>  
+                  <TextInput style={{borderWidth:1,margin:10}} placeholder="Num1" onChangeText={Num1->this.setState({Num1})} ></TextInput>
+                   <TextInput style={{borderWidth:1,margin:10}} placeholder="Num2" onChangeText={Num2->this.setState({Num2})}></TextInput>
+                   <Button title="Sum" onPress={this.F1}/>  
             </View>
         );
     }
