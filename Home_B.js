2791d8f0f77de44790e2be1d491032dd41e176ae
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-05-15 13:13:58 +0530

    bottom navigation

--- Home_B.js
+++ Home_B.js
@@ -1,7 +1,14 @@
 import React,{Component} from 'react';
 import {View,Text} from 'react=native';
+import {createBottomTabNavigator,createAppContainer} from 'react-navigation';
+import {createMaterialBottomTabNavigator} from 'react-navigation-material-bottom-tabs';
+import {Icon} from 'react-native-elements';
 
-export default class Home_B extends Component
+import Profile from './Profile';
+import Cart from './Cart';
+
+
+class Home_B extends Component
 {
 
     render()
@@ -14,3 +21,53 @@ export default class Home_B extends Component
         );
     }
 }
+
+const TabNavigator=createMaterialBottomTabNavigator(
+    {
+        Home_B:{screen:Home_B,
+        navigationOptions:{
+            tabBarLabel:'Home_B',
+            activeColor:'#ff0000',
+            inactiveColor:'#000000',
+            barStyle:{ backgroundColor:'#67baf6'},
+            tabBarIcon:()=>(
+                <View>
+                    <Icon name={'home'} size={25} style={{color:'#ff0000'}}/>
+                </View>
+            )
+
+        }
+        },
+
+         Profile:{screen:Profile,
+        navigationOptions:{
+            tabBarLabel:'Profile',
+            activeColor:'#ff0000',
+            inactiveColor:'#000000',
+            barStyle:{ backgroundColor:'#67baf6'},
+            tabBarIcon:()=>(
+                <View>
+                    <Icon name={'person'} size={25} style={{color:'#ff0000'}}/>
+                </View>
+            )
+
+        }
+        },
+         Cart:{screen:Cart,
+        navigationOptions:{
+            tabBarLabel:'Cart',
+            activeColor:'#ff0000',
+            inactiveColor:'#000000',
+            barStyle:{ backgroundColor:'#67baf6'},
+            tabBarIcon:()=>(
+                <View>
+                    <Icon name={'add-shopping-cart'} size={25} style={{color:'#ff0000'}}/>
+                </View>
+            )
+
+        }
+        }
+
+        export default createAppContainer(TabNavigator);
+    }
+);
